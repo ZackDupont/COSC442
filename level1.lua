d361599624eeeab61b296a0b8439385db31fd654
zdupo067 <zack_dupont@hotmail.com>
2017-10-22 19:11:03 -0700

    Add files via upload

--- level1.lua
+++ level1.lua
@@ -35,7 +35,7 @@ function scene:create( event )
 	-- since we are going to position the background from it's top, left corner, draw the
 	-- background at the real top, left corner.
 
-	local background = display.newImageRect( "waterbg.jpg", display.actualContentWidth, display.actualContentHeight )
+	local background = display.newImageRect( "images/waterbg.jpg", display.actualContentWidth, display.actualContentHeight )
 	background.anchorX = 0
 	background.anchorY = 0
 	background.x = 0 + display.screenOriginX
@@ -93,55 +93,55 @@ end
 
 -- FISHES
 
-local fish = display.newImageRect("fish.png", 60, 60)
-fish.x = 1000
-fish.y = 200
+local fishA = display.newImageRect("images/fish.png", 200, 200)
+fishA.x = 700
+fishA.y = 3000
 
-local fish1 = display.newImageRect("BPFish.png", 60, 60)
-fish1.x = -500
-fish1.y = 275
+local fish1 = display.newImageRect("images/BPFish.png", 200, 200)
+fish1.x = 500
+fish1.y = -500
 
-local fish2 = display.newImageRect("GFish.png", 60, 60)
-fish2.x = 1000
-fish2.y = 300
+local fish2 = display.newImageRect("images/GFish.png", 200, 200)
+fish2.x = 600
+fish2.y = 3000
 
-local fish3 = display.newImageRect("RFish.png", 60, 60)
-fish3.x = -500
-fish3.y = 150
+local fish3 = display.newImageRect("images/RFish.png", 200, 200)
+fish3.x = 300
+fish3.y = -1000
 
-local fish4 = display.newImageRect("YFish.png", 60, 60)
-fish4.x = 1000
-fish4.y = 175
+local fish4 = display.newImageRect("images/YFish.png", 200, 200)
+fish4.x = 420
+fish4.y = 3000
 
 -- MOVING FISHES
 
 local function moveFishA()
-  transition.to(fishA, {x = -100, y = 200, time=9000,onComplete=function() fishA.x = 1000 fishA.y = 200
-    transition.to(fishA, {x = -100, y = 200, time=9000, onComplete=moveFish()})
+  transition.to(fishA, {x = 700, y = -100, time=9000,onComplete=function() fishA.x = 700 fishA.y = 3000
+    transition.to(fishA, {x = 700, y = -100, time=9000, onComplete=moveFishA()})
   end})
 end
 
 local function moveFish1()
-  transition.to(fish1, {x = 700, y = 275, time=10000,onComplete=function() fish1.x = -500 fish1.y = 275
-    transition.to(fish1, {x = 700, y = 275, time=10000, onComplete=moveFish1()})
+  transition.to(fish1, {x = 500, y = 2700, time=10000,onComplete=function() fish1.x = 500 fish1.y = -1000
+    transition.to(fish1, {x = 500, y = 2700, time=10000, onComplete=moveFish1()})
   end})
 end
 
 local function moveFish2()
-  transition.to(fish2, {x = -100, y = 300, time=12000,onComplete=function() fish2.x = 1000 fish2.y = 300
-    transition.to(fish2, {x = -100, y = 300, time=12000, onComplete=moveFish2()})
+  transition.to(fish2, {x = 600, y = -1000, time=12000,onComplete=function() fish2.x = 600 fish2.y = 3000
+    transition.to(fish2, {x = 600, y = -1000, time=12000, onComplete=moveFish2()})
   end})
 end
 
 local function moveFish3()
-  transition.to(fish3, {x = 700, y = 150, time=15000,onComplete=function() fish3.x = -500 fish1.y = 150
-    transition.to(fish3, {x = 700, y = 150, time=15000, onComplete=moveFish3()})
+  transition.to(fish3, {x = 300, y = 3000, time=15000,onComplete=function() fish3.x = 300 fish1.y = -1000
+    transition.to(fish3, {x = 300, y = 1000, time=15000, onComplete=moveFish3()})
   end})
 end
 
 local function moveFish4()
-  transition.to(fish4, {x = -100, y = 175, time=14000,onComplete=function() fish4.x = 1000 fish1.y = 175
-    transition.to(fish4, {x = -100, y = 175, time=14000, onComplete=moveFish4()})
+  transition.to(fish4, {x = 420, y = -1000, time=14000,onComplete=function() fish4.x = 420 fish1.y = 3000
+    transition.to(fish4, {x = 420, y = -1000 , time=14000, onComplete=moveFish4()})
   end})
 end
 
@@ -149,12 +149,88 @@ end
 
 for i=1,10,1
 do
-  moveFish()
+  moveFishA()
   moveFish1()
   moveFish2()
   moveFish3()
   moveFish4()
 end
+
+local widget = require("widget")
+local physics = require("physics")
+
+physics.start()
+physics.setGravity(0, 0)
+
+
+
+
+local backGroup = display.newGroup()
+local mainGroup = display.newGroup()
+local uiGroup = display.newGroup()
+
+local score = display.newText(uiGroup, "F I S H ", 1300, 2000, native.systemFont, 240)
+score.rotation = 90
+score:setFillColor(0,0,0)
+
+ local score = display.newText(uiGroup, "That's right! The correct word is F I S H! ", 700, 1300, native.systemFont, 120)
+ score.rotation = 90
+ score:setFillColor(0,0,0)
+
+
+
+local boat = display.newImageRect(mainGroup, "images/boat.png", 400, 800)
+        boat.x = display.contentCenterX + 222
+        boat.y = display.contentCenterY - 100
+        physics.addBody( boat, {radius = 30, isSensor = true })
+        boat.myName = boat
+
+local monkey = display.newImageRect(mainGroup, "images/monkey.png", 600, 600)
+        monkey.x = boat.x + 128
+        monkey.y = boat.y + 280
+
+
+ local fLine = display.newRect(mainGroup, monkey.x + 20, monkey.y, 30, 150)
+        fLine:setFillColor(brown)
+        -- fLine:toBack()
+fLine.rotation = 90
+
+local function lowerLineRelease (event)
+        fLine.x = fLine.x - 30
+        fLine.height = fLine.height + 60
+        return true
+end
+
+local function raiseLineRelease (event)
+        fLine.x = fLine.x + 30
+        fLine.height = fLine.height - 60
+        return true
+end
+
+local upButton = widget.newButton
+{
+        left = 120,
+        top = 2300,
+        width = 300,
+        height = 150,
+        defaultFile = "images/upButton.png",
+        overFile = "images/upButton.png",
+        label = "Raise Line",
+        onRelease = raiseLineRelease,
+}
+upButton.rotation = 90
+
+local downButton = widget.newButton{
+        left = 0,
+        top = 2300,
+        width = 300,
+        height = 150,
+        defaultFile = "images/downButton.png",
+        overFile = "images/downButton.png",
+        label = "Lower Line",
+        onRelease = lowerLineRelease,
+}
+downButton.rotation = 90
 
 
 
